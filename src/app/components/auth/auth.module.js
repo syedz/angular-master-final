80021cdf1595d4209d18ec8c8d7d37078eeac3b3
Zeeshan Syed <zsyed89@gmail.com>
2016-09-27 23:43:12 -0400

    Added firebase configuration

--- src/app/components/auth/auth.module.js
+++ src/app/components/auth/auth.module.js
@@ -2,4 +2,21 @@ angular
   .module('components.auth', [
     'ui.router',
     'firebase'
-  ]);
+  ])
+  .config(function($firebaseRefProvider) {
+    var config = {
+        apiKey:            "AIzaSyCSJnZlcMfNKXRniisNeVf_CsIXa0MJQzs",
+        authDomain:        "contacts-manager-76cfd.firebaseapp.com",
+        databaseURL:       "https://contacts-manager-76cfd.firebaseio.com",
+        storageBucket:     "contacts-manager-76cfd.appspot.com",
+        messagingSenderId: "633681611585"
+    };
+
+    $firebaseRefProvider
+        .registerUrl({
+            default: config.databaseURL,
+            contacts: config.databaseURL + '/contacts'
+        });
+
+    firebase.initializeApp(config);
+  });
