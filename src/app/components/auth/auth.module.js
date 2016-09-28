5490dde74a83bdcd348cb391bdb766bf746aedc3
Zeeshan Syed <zsyed89@gmail.com>
2016-09-28 09:44:15 -0400

    Added login and register components

--- src/app/components/auth/auth.module.js
+++ src/app/components/auth/auth.module.js
@@ -1,16 +1,16 @@
 angular
-  .module('components.auth', [
-    'ui.router',
-    'firebase'
-  ])
-  .config(function($firebaseRefProvider) {
-    var config = {
-        apiKey:            "AIzaSyCSJnZlcMfNKXRniisNeVf_CsIXa0MJQzs",
-        authDomain:        "contacts-manager-76cfd.firebaseapp.com",
-        databaseURL:       "https://contacts-manager-76cfd.firebaseio.com",
-        storageBucket:     "contacts-manager-76cfd.appspot.com",
-        messagingSenderId: "633681611585"
-    };
+    .module('components.auth', [
+        'ui.router',
+        'firebase'
+    ])
+    .config(function($firebaseRefProvider) {
+        var config = {
+            apiKey:            "AIzaSyCSJnZlcMfNKXRniisNeVf_CsIXa0MJQzs",
+            authDomain:        "contacts-manager-76cfd.firebaseapp.com",
+            databaseURL:       "https://contacts-manager-76cfd.firebaseio.com",
+            storageBucket:     "contacts-manager-76cfd.appspot.com",
+            messagingSenderId: "633681611585"
+        };
 
     $firebaseRefProvider
         .registerUrl({
