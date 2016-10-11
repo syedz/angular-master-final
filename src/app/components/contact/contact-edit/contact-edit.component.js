2409de7e43b450212cfd7284f71cf89b32c49746
Zeeshan Syed <zsyed89@gmail.com>
2016-10-11 08:42:53 -0400

    Added more comments

--- src/app/components/contact/contact-edit/contact-edit.component.js
+++ src/app/components/contact/contact-edit/contact-edit.component.js
@@ -1,5 +1,10 @@
 /*
-  Our first stateful routed component.
+  Our first stateful routed component. Up until this point, we only had
+  routed components that did not get data as we transitioned to them. Edit
+  functionality requires us to have data before being utilized.
+
+  In state.go() we take the key from the user and put it in the URL, when we
+  start transitioning to this new state we read that value from the URL.
  */
 
 var contactEdit = {
@@ -19,8 +24,10 @@ angular
         parent: 'app', // This make this route protected by authentication
         url: '/contact/:id',
         component: 'contactEdit',
-        resolve: {
+        resolve: { // Need to figure out how to resolve the data, before getting it into the component by resolving that data before we transition
           contact: function($transition$, ContactService) {
+            // Here we return the particular contact before transitioning into the view so that we get it as a binding.
+            // $transition$ is required so that we can fetch the actual URL value, so we can pass it off to our service
             var key = $transition$.params().id;
             console.log(key);
             return ContactService.getContactById(key).$loaded();
